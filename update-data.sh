cc66652d53649636b937f77049b297cec9449e97
Steve Mao <maochenyan@gmail.com>
2021-10-09 17:56:43 +1100

    update data

---
+++ update-data.sh
@@ -0,0 +1,2 @@
+curl https://raw.githubusercontent.com/ElMustacho/catbot-v1.1/master/enemyunits.tsv --output ./data/enemyunits.tsv
+curl https://raw.githubusercontent.com/ElMustacho/catbot-v1.1/master/stages10.2.db --output ./data/stages10.2.db
