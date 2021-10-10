ef2ff9f18084e8d774d70a4a31b5c8ed3cd2841a
Steve Mao <maochenyan@gmail.com>
2021-10-10 19:20:02 +1100

    add enemy icons

--- update-data.sh
+++ update-data.sh
@@ -1,2 +1,14 @@
 curl https://raw.githubusercontent.com/ElMustacho/catbot-v1.1/master/enemyunits.tsv --output ./data/enemyunits.tsv
 curl https://raw.githubusercontent.com/ElMustacho/catbot-v1.1/master/stages10.2.db --output ./data/stages10.2.db
+
+cd bcu-resources
+git pull
+cd resources/assets
+for FILE in *; 
+    do unzip -o $FILE; 
+done
+cd ../../..
+mkdir -p ./data/enemy
+mv bcu-resources/resources/assets/org/enemy/**/enemy_icon_*.png ./data/enemy
+cd bcu-resources
+git clean -fdx
