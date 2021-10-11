d31a3da09e9bdc4215530eaf3228388782f325d8
Steve Mao <maochenyan@gmail.com>
2021-10-11 23:33:05 +1100

    turn into command line

--- README.md
+++ README.md
@@ -1,27 +1,13 @@
 # monthly-missions
 
-> Find the most energy efficient stages
+> Find the most energy efficient stages for your monthly missions
+
+Manually find the stages that contain the enemies can be a very time consuming task
+This tool can find your enemies very quickly
+You might even find multiple enemies in one stage
 
 ## How to use this tool
 
-### Clone this repo
-
-```
-git clone stevemao/monthly-missions
-```
-
-### Install stack
-
-```
-brew install haskell-stack
-```
-
-### Set your missions
-
-Change your missions in app/Main.hs
-
-### Run it
-
-```
-stack run
-```
+1. download 
+1. run `monthly-missions sol "St. Pigge the 2nd" sol "Owlbrow"`
+1. for more information, check `monthly-missions --help`
--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,19 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE OverloadedStrings  #-}
 module Main where
 
 import           BattleCats.Image
 import           BattleCats.MonthlyMissions.Lib
 import           BattleCats.MonthlyMissions.Types
+import           Control.Exception
 import           Control.Monad.IO.Class
 import qualified Data.ByteString                   as BS
 import           Data.Foldable
-import           Data.List.NonEmpty                (NonEmpty ((:|)))
+import           Data.List.NonEmpty                (nonEmpty)
+import qualified Data.Text                         as T
+import           Data.Version
+import           Paths_monthly_missions
+import           System.Console.CmdArgs
 import           Text.Pretty.Simple
 import           Text.Pretty.Simple.Internal.Color
 
@@ -21,26 +27,63 @@ cotcCh2 = LocationLevel "CotC Ch.2"
 cotcCh3 = LocationLevel "CotC Ch.3"
 sol = LocationCategory "SoL"
 
+data Missions
+  = Missions
+      { m :: [T.Text]
+      }
+  deriving (Data, Show, Typeable)
+
+input :: Missions
+input = Missions (def &= args &= typ "location target...") &= program "monthly-missions" &=
+           help "Find the most energy efficient stages for your monthly missions" &=
+           summary ("monthly-missions " <> showVersion version <> ", (C) Steve Mao") &=
+           details ["Examples:"
+                   , "  monthly-missions cotcCh2 'Cat God' cotcCh2 'Shibalien Elite'"
+                   , "  monthly-missions sol 'Shibalien Elite' sol 'Star Peng'", ""
+                   , "Manually find the stages that contain the enemies can be a very time consuming task",""
+                   ,"This tool can find your enemies very quickly","  You might even find multiple enemies in one stage"
+                   ]
+
+pairs :: [b] -> [(b, b)]
+pairs []           = []
+pairs [_]          = []
+pairs (a : b : as) = [(a,b)] <> pairs as
+
+fromString :: T.Text -> Location
+fromString "eocCh2"  = LocationLevel "EoC Ch.2"
+fromString "eocCh3"  = LocationLevel "EoC Ch.3"
+fromString "itfCh1"  = LocationLevel "ItF Ch.1"
+fromString "itfCh2"  = LocationLevel "ItF Ch.2"
+fromString "cotcCh1" = LocationLevel "CotC Ch.1"
+fromString "cotcCh2" = LocationLevel "CotC Ch.2"
+fromString "cotcCh3" = LocationLevel "CotC Ch.3"
+fromString "sol"     = LocationCategory "SoL"
+fromString l         = LocationLevel (Level l)
+
 main :: IO ()
 main = do
-  let missions = Mission sol (Target "Owlbrow") :|
-               [ Mission sol (Target "St. Pigge the 2nd") ]
+  as <- cmdArgs input
 
-  MinEnergyStages stages <- getMinStages missions
+  let ms = (\(l, e) -> Mission (fromString l) (Target e)) <$> pairs (m as)
 
-  traverse_ (\(stage, enemies) -> do
-      BS.putStr "\n"
+  case nonEmpty ms of
+    Nothing -> throwIO (error "You must specify at least one location and enemy. try monthly-missions cotcCh2 'Cat God' cotcCh2 'Shibalien Elite'" :: SomeException)
+    Just missions -> do
+      MinEnergyStages stages <- getMinStages missions
 
-      pPrint stage
+      traverse_ (\(stage, enemies) -> do
+          BS.putStr "\n"
 
-      traverse (\enemy@(Enemy _ _ _ code) -> do
-            pPrintIndented enemy
+          pPrint stage
 
-            BS.putStr "         "
-            terminalImage code
-            BS.putStr "\n"
-        ) enemies
-      ) stages
+          traverse (\enemy@(Enemy _ _ _ code) -> do
+                pPrintIndented enemy
+
+                BS.putStr "         "
+                terminalImage code
+                BS.putStr "\n"
+            ) enemies
+          ) stages
 
 pPrintIndented :: (MonadIO m, Show a) => a -> m ()
 pPrintIndented = pPrintOpt CheckColorTty defaultOutputOptionsDarkBg
