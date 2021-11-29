53ea941f15886e63ba652270c72c004afa3820f7
Steve Mao <stevemao@Steves-MacBook-Pro.local>
2021-11-29 15:40:43 +1100

    demo

--- src/Data/List/NonEmpty/Utils.hs
+++ src/Data/List/NonEmpty/Utils.hs
@@ -1,26 +1,27 @@
 module Data.List.NonEmpty.Utils where
-import           Data.List
-import           Data.List.NonEmpty (NonEmpty ((:|)), toList)
+
+import Data.List
+import Data.List.NonEmpty (NonEmpty ((:|)), toList)
 
 upsertList :: Eq k => (Maybe v -> v) -> k -> [(k, v)] -> NonEmpty (k, v)
 upsertList f k m = case findValueIndex (\(key, _) -> key == k) m of
-  Nothing              -> (k, f Nothing) :| m
+  Nothing -> (k, f Nothing) :| m
   Just ((_, v), i, ne) -> replace i (k, f (Just v)) ne
 
 findValueIndex :: (a -> Bool) -> [a] -> Maybe (a, Int, NonEmpty a)
 findValueIndex _ [] = Nothing
-findValueIndex f x@(a : as) = case find (f . fst) (zip x [0..]) of
-  Nothing     -> Nothing
+findValueIndex f x@(a : as) = case find (f . fst) (zip x [0 ..]) of
+  Nothing -> Nothing
   Just (b, i) -> Just (b, i, a :| as)
 
 replace :: Int -> a -> NonEmpty a -> NonEmpty a
-replace 0 a (_:|xs) = a :| xs
-replace n a (x:|(x':xs)) =
+replace 0 a (_ :| xs) = a :| xs
+replace n a (x :| (x' : xs)) =
   if n < 0
-    then x :| (x':xs)
-    else x :| toList (replace (n-1) a (x' :| xs))
-replace _ _ (x:|[]) = x :| []
+    then x :| (x' : xs)
+    else x :| toList (replace (n -1) a (x' :| xs))
+replace _ _ (x :| []) = x :| []
 
-foldr2NonEmpty :: (a -> [b] -> NonEmpty b) -> NonEmpty a -> NonEmpty b
-foldr2NonEmpty f (a :| [])       = f a []
-foldr2NonEmpty f (a :| (b : bs)) = f a $ toList $ foldr2NonEmpty f (b :| bs)
+foldr2NonEmpty :: (a -> [a] -> NonEmpty a) -> NonEmpty a -> NonEmpty a
+foldr2NonEmpty f (a :| []) = f a []
+foldr2NonEmpty f (a :| (b : bs)) = f a (toList (f b bs))
--- test/Data/List/NonEmpty/UtilsSpec.hs
+++ test/Data/List/NonEmpty/UtilsSpec.hs
@@ -1,17 +1,18 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE OverloadedStrings          #-}
-{-# LANGUAGE StandaloneDeriving         #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
 module Data.List.NonEmpty.UtilsSpec (main, spec) where
 
-import           Data.Foldable
-import           Data.List.NonEmpty        (NonEmpty)
-import qualified Data.List.NonEmpty        as NE
-import           Data.List.NonEmpty.Utils
-import           Data.Maybe
-import           Safe
-import           Test.Hspec
-import           Test.QuickCheck
-import           Test.QuickCheck.Instances ()
+import Data.Foldable
+import Data.List.NonEmpty (NonEmpty)
+import qualified Data.List.NonEmpty as NE
+import Data.List.NonEmpty.Utils
+import Data.Maybe
+import Safe
+import Test.Hspec
+import Test.QuickCheck
+import Test.QuickCheck.Instances ()
 
 -- `main` is here so that this module can be run from GHCi on its own.  It is
 -- not needed for automatic spec discovery.
@@ -36,32 +37,39 @@ prop_emptyList f = isNothing $ findValueIndex (applyFun f) []
 
 prop_nonEmptyList :: Fun Integer Bool -> NonEmpty Integer -> Bool
 prop_nonEmptyList f as = case findValueIndex f' $ NE.toList as of
-    Nothing              -> isNothing $ find f' as
-    Just (a, index, as') -> find f' as == Just a && index >= 0 && as' == as
-    where f' = applyFun f
+  Nothing -> isNothing $ find f' as
+  Just (a, index, as') -> find f' as == Just a && index >= 0 && as' == as
+  where
+    f' = applyFun f
 
 prop_positive_replace :: NonNegative Int -> Integer -> NonEmpty Integer -> Bool
-prop_positive_replace index a as = length as == length as' && if index' >= length as
-                          then as == as'
-                          else atMay (NE.toList as') index' == Just a
-    where as' :: NonEmpty Integer
-          as' = replace index' a as
-          index' = getNonNegative index
+prop_positive_replace index a as =
+  length as == length as'
+    && if index' >= length as
+      then as == as'
+      else atMay (NE.toList as') index' == Just a
+  where
+    as' :: NonEmpty Integer
+    as' = replace index' a as
+    index' = getNonNegative index
 
 prop_negative_replace :: Negative Int -> Integer -> NonEmpty Integer -> Bool
 prop_negative_replace index a as = as == as'
-    where as' :: NonEmpty Integer
-          as' = replace index' a as
-          index' = getNegative index
+  where
+    as' :: NonEmpty Integer
+    as' = replace index' a as
+    index' = getNegative index
 
 prop_upsertList :: Fun (Maybe Integer) Integer -> Int -> [(Int, Integer)] -> Bool
 prop_upsertList f k m = case lookup k m of
-            Nothing -> (k, f' Nothing) `elem` m'
-            Just v  -> length m' == length m && elem (k, f' (Just v)) m'
-            where f' = applyFun f
-                  m' = upsertList f' k m
+  Nothing -> (k, f' Nothing) `elem` m'
+  Just v -> length m' == length m && elem (k, f' (Just v)) m' && notElem (k, v) m'
+  where
+    f' = applyFun f
+    m' = upsertList f' k m
 
 prop_foldr2NonEmpty :: Fun (Integer, [Integer]) (NonEmpty Integer) -> NonEmpty Integer -> Bool
 prop_foldr2NonEmpty f as = NE.toList (foldr2NonEmpty f' as) == foldr f'' [] (NE.toList as)
-            where f' = applyFun2 f
-                  f'' a b = NE.toList $ f' a b
+  where
+    f' = applyFun2 f
+    f'' a b = NE.toList $ f' a b
