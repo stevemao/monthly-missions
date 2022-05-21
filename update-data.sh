9abb1c25d8606c2bf945ff449155780982eda750
Steve Mao <maochenyan@gmail.com>
2022-05-21 19:14:23 +1000

    fix

--- update-data.sh
+++ update-data.sh
@@ -1,9 +1,12 @@
+set -e
+
 mkdir -p ./data/enemy
 curl --fail https://raw.githubusercontent.com/ElMustacho/catbot-v1.1/master/enemyunits.tsv --output ./data/enemyunits.tsv
 curl --fail https://raw.githubusercontent.com/ElMustacho/catbot-v1.1/master/stages.db --output ./data/stages.db
 
+git submodule init
+git submodule update
 cd bcu-resources
-git pull
 cd resources/assets
 for FILE in *; 
     do unzip -o $FILE; 
